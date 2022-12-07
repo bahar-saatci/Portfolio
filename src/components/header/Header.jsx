f28a0b47fca8c10c9a18c6814bbe8e0d5af165ce
Manea-Alex <alexmanea2201@gmail.com>
2022-12-07 20:20:40 +0200

    deploy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
-import Alex from "../../assets/Portofoliu5.jpg";
+import Bahar from "../../assets/Avatar4.png";
 import HeaderSocial from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Szőke-Manea Alexandru </h1>
-        <h5 className="text-light">Student</h5>
+        <h1>Saatçi Bahar </h1>
+        <h5 className="text-light">
+          Student, Architect, Engineer, Mentor, Lover, Mother, Wife
+        </h5>
         <CTA />
         <HeaderSocial />
 
         <div className="me">
-          <img src={Alex} alt="me" />
+          <img src={Bahar} alt="me" />
         </div>
         <a href="#contact" className="scroll__down">
           Scroll Down
